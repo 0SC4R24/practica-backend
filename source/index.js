b8a3db4281f76022872161f475102096729b18ad
0SC4R24 <oscarviudez24@gmail.com>
2024-05-14 04:05:39 +0200

    Slack working
     - Added to .env WEBHOOK
    
     - Sends messages above code 4XX

--- source/index.js
+++ source/index.js
@@ -2,12 +2,24 @@ const express = require("express")
 const cors = require("cors")
 const router = require("./routes")
 const dbConnect = require("./config/mongo")
+const morganBody = require("morgan-body")
 
 require("dotenv").config()
+const loggerStream = require("./utils/handleLogger")
+
 dbConnect()
 
 const app = express()
 const port = process.env.PORT || 3000
+
+morganBody(app, {
+    noColors: true,
+    skip: function (req, res)
+    {
+        return res.statusCode < 400
+    },
+    stream: loggerStream
+})
 
 app.use(express.json())
 app.use(cors())
